7d698d3478582243b7f2f05ba8f8bd50e8498c8f
VuDiep1306 <vudiep.1306@gmail.com>
2025-09-25 14:43:11 +0900

    Add files via upload

---
+++ session10_3.js
@@ -0,0 +1,3 @@
+let width = Number(prompt("Chiều dài của hình chữ nhật là: "));
+let height = Number(prompt("Chiều rộng của hình chữ nhật là: "));
+console.log("Diện tích của hình chữ nhật là: " + width * height); 
