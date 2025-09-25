a8b20f8c0f006b88ad9c6d3469e81484aeb463c3
VuDiep1306 <vudiep.1306@gmail.com>
2025-09-25 15:30:13 +0900

    Add files via upload

---
+++ session10_6.js
@@ -0,0 +1,4 @@
+let x = Number(prompt("Hãy nhập điểm môn Math: "));
+let y = Number(prompt("Hãy nhập điểm môn Physics: "));
+let z = Number(prompt("Hãy nhập điểm môn Chemistry: "));
+console.log("Điểm trung bình là: " + (x+y+z)/3);
